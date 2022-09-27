288703bb62dc174edafa940889226d36e87641e2
VictorHugo1607ESIME <victor.hugo.mr1607@gmail.com>
2022-09-27 16:53:37 -0500

    new message "Start"

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        print("Start")
         initViewModel()
         setContentView(binding!!.root)
     }
